96a3e0909599589dbaf058b237300e772532e2f5
undefined <undefined>
2014-02-26 03:40:59 +0900

    add withCurrent

--- Graphics/EGL.hs
+++ Graphics/EGL.hs
@@ -392,6 +392,17 @@ swapCurrentBuffers = do
   surf <- eglGetCurrentSurface EGLDraw
   either return (\disp -> either return (\surf -> eglSwapBuffers disp surf) surf) disp
 
+withCurrent f g = do
+  disp <- eglGetCurrentDisplay
+  cont <- eglGetCurrentContext
+  read <- eglGetCurrentSurface EGLRead
+  draw <- eglGetCurrentSurface EGLDraw
+  either f (\d->
+    either f (\c->
+      either f (\r->
+        either f (\s->
+          g d c r s) draw) read) cont) disp
+
 -- * Types
 -- from EGL/eglplatform.h
 -- ** Window-system-dependent types
