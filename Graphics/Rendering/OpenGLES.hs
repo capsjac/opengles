b36bb0d0f9a2930681e2f19256a8a30369927c5c
undefined <undefined>
2014-05-14 18:53:44 +0900

    add OpenGL ES 2+3 wrapper APIs

--- Graphics/Rendering/OpenGLES.hs
+++
@@ -1,7 +0,0 @@
-module Graphics.Rendering.OpenGL (
---	module Graphics.Rendering.OpenGL.Types
-) where
---import Graphics.Rendering.OpenGL.Types
--- OpenGL ES (ES for Embed Systems)
--- ANGLE: OpenGL ES 2.0 backend for Windows.
--- http://code.google.com/p/angleproject/
